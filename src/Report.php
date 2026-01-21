3005f43fb5508e373d82ded63181e48fc3280547
Akihito Koriyama <akihito.koriyama@gmail.com>
2026-01-21 10:05:03 +0900

    Show n/a for DevelopmentCodeFragment values

--- src/Report.php
+++ src/Report.php
@@ -388,9 +388,10 @@ final class Report
                 $lineInfo = $issue['line'] > 0 ? ":{$issue['line']}" : '';
                 $copyPath = htmlspecialchars($shortPath) . $lineInfo;
                 $metricLink = $this->getMetricLink($issue['metric'], 'html');
+                $displayValue = $issue['metric'] === 'DevelopmentCodeFragment' ? 'n/a' : (string) $issue['value'];
                 $output .= "<div class=\"issue-row\">\n";
                 $output .= "<span class=\"issue-name\">{$metricLink}</span>\n";
-                $output .= "<span class=\"issue-value\">{$issue['value']}</span>\n";
+                $output .= "<span class=\"issue-value\">{$displayValue}</span>\n";
                 $output .= "<span class=\"issue-threshold\">({$thresholds})</span>\n";
                 $output .= "<span class=\"issue-copy\" onclick=\"copyPath(this, '{$copyPath}')\">" . self::COPY_ICON_SVG . "</span>\n";
                 $output .= "</div>\n";
@@ -424,10 +425,11 @@ final class Report
                 $lineInfo = $f['line'] > 0 ? ":L{$f['line']}" : '';
                 $copyPath = htmlspecialchars($f['path']) . ($f['line'] > 0 ? ":{$f['line']}" : '');
                 $lvl = self::LEVELS[$f['level']];
+                $displayValue = $metric === 'DevelopmentCodeFragment' ? 'n/a' : (string) $f['value'];
                 $output .= "<div class=\"issue-file-row\">\n";
                 $output .= '<span class="issue-file-path">' . htmlspecialchars($f['path']) . "{$lineInfo}</span>\n";
                 $output .= "<span class=\"issue-file-right\">\n";
-                $output .= "<span class=\"issue-file-value\">{$f['value']}</span>\n";
+                $output .= "<span class=\"issue-file-value\">{$displayValue}</span>\n";
                 $output .= "<span class=\"level-badge-mini level-{$f['level']}\">{$lvl['emoji']}</span>\n";
                 $output .= "<span class=\"issue-file-copy\" onclick=\"copyPath(this, '{$copyPath}')\">" . self::COPY_ICON_SVG . "</span>\n";
                 $output .= "</span>\n";
